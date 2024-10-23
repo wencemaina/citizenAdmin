f4a01cf5c7b22423419b275180abeb1a6d14b16f
wencemaina <89692434+wencemaina@users.noreply.github.com>
2024-10-24 01:34:15 +0300

    add padding and change to ksh as currency

--- components/create-product/pricing.jsx
+++ components/create-product/pricing.jsx
@@ -7,7 +7,7 @@ export const Pricing = ({ formData, handleChange }) => (
 			</label>
 			<div className="mt-1 relative rounded-md shadow-sm">
 				<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
-					<span className="text-gray-500 sm:text-sm">$</span>
+					<span className="text-gray-500 sm:text-sm">Ksh</span>
 				</div>
 				<input
 					type="number"
@@ -16,7 +16,7 @@ export const Pricing = ({ formData, handleChange }) => (
 					onChange={handleChange}
 					min="0"
 					step="0.01"
-					className="mt-1 block w-full rounded-md border border-gray-300 pl-7 px-3 py-2 text-sm focus:border-blue-500 focus:ring-blue-500"
+					className="mt-1  block w-full rounded-md border border-gray-300 pl-10 px-3 py-2 text-sm focus:border-blue-500 focus:ring-blue-500"
 					required
 				/>
 			</div>
